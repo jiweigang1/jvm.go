3977b909166ad73288c1fc8835f11ef9d156802a
冀刚 <jiweigang2008@126.com>
2020-04-08 14:55:19 +0800

    注释
    注释

--- instructions/references/invokestatic.go
+++ instructions/references/invokestatic.go
@@ -11,19 +11,24 @@ type InvokeStatic struct {
 	base.Index16Instruction
 	method *heap.Method
 }
-
+//调用静态方法
 func (instr *InvokeStatic) Execute(frame *rtda.Frame) {
+	//如果关联的方法为空的化进行关联，静态方法可以直接从符号应用进行关联的，静态方法不存在覆盖的问题可以直接获取到指定的方法
 	if instr.method == nil {
+		//获取常量池
 		cp := frame.GetConstantPool()
 		k := cp.GetConstant(instr.Index)
+		//如果是类的静态方法
 		if kMethodRef, ok := k.(*heap.ConstantMethodRef); ok {
 			instr.method = kMethodRef.GetMethod(true)
+		//如果是接口的构造方法
 		} else {
 			instr.method = k.(*heap.ConstantInterfaceMethodRef).GetMethod(true)
 		}
 	}
 
 	// init class
+	//如果class没有初始化，初始化class
 	class := instr.method.Class
 	if class.InitializationNotStarted() {
 		frame.RevertNextPC()
